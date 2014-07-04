987c5561c83be954aa6a91daf517cc7f1ef57a85
Mitar <mitar.git@tnode.com>
2014-07-04 16:40:38 -0700

    Added blocking function for direct access to the database.

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.use(['coffeescript', 'underscore', 'logging', 'minimongo', 'util', 'assert', 'moment', 'stacktrace'], ['client', 'server']);
+  api.use(['coffeescript', 'underscore', 'minimongo', 'assert', 'stacktrace'], ['client', 'server']);
 
   api.export('Document');
 
@@ -11,19 +11,23 @@ Package.on_use(function (api) {
     'lib.coffee'
   ], ['client', 'server']);
 
+  api.use(['logging', 'random', 'util', 'moment', 'blocking', 'ejson'], 'server');
   api.add_files([
+    'direct.coffee',
     'server.coffee'
   ], 'server');
 });
 
 Package.on_test(function (api) {
-  api.use(['peerdb', 'tinytest', 'test-helpers', 'coffeescript', 'insecure', 'accounts-base', 'accounts-password', 'assert'], ['client', 'server']);
+  api.use(['peerdb', 'tinytest', 'test-helpers', 'coffeescript', 'insecure', 'accounts-base', 'accounts-password', 'assert', 'blocking', 'underscore'], ['client', 'server']);
   api.add_files([
     'tests_defined.js',
     'tests.coffee'
   ], ['client', 'server']);
 
   api.add_files([
+    'direct.coffee',
+    'tests_direct.coffee',
     'tests_migrations.coffee'
   ], 'server');
 });
