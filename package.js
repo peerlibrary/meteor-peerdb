31f7048b0cea2db08be9a473a7ade870b516538d
Mitar <mitar.git@tnode.com>
2016-08-19 21:00:32 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'peerlibrary:peerdb',
   summary: "Reactive database layer with references, generators, triggers, migrations, etc.",
-  version: '0.21.0',
+  version: '0.22.0',
   git: 'https://github.com/peerlibrary/meteor-peerdb.git'
 });
 
